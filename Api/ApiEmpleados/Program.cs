cab8fa4aed5f1173a57741df8cdfeed17e744aab
Nicolas Fiquitiva Segura <Nico52105@gmail.com>
2025-01-27 13:35:02 -0500

    se agrega politica de CORS

--- Api/ApiEmpleados/Program.cs
+++ Api/ApiEmpleados/Program.cs
@@ -8,6 +8,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+    {
+        options.AddPolicy(name: "CorsPolicy",
+            builder =>
+            {
+                builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+            }
+        );
+    });
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -20,6 +29,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors("CorsPolicy");
 
 app.MapControllers();
 
